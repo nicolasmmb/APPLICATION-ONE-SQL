73a7fb9751be04a98df5fe03263ef1c1c212fdce
NICK <nicolas.mmb@hotmail.com>
2021-11-14 12:44:54 -0300

    Modifications:  - New Routes  - New Verifications  - Garbage Remotions

--- app/routes/address.py
+++ app/routes/address.py
@@ -12,12 +12,18 @@ import time
 
 router = APIRouter(
     prefix="/address",
-    tags=['ADDRESS']
+    #tags=['ADDRESS']
 )
 
 
-@router.post('/create', response_model=AddressCreate, status_code=status.HTTP_201_CREATED)
+@router.post('/create', response_model=AddressCreate, status_code=status.HTTP_201_CREATED, tags=['ADDRESS'])
 async def create_address(address: AddressCreate, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+    print('user_data: '.upper() + str(user_data.id))
+
+    address_exists = db.query(Address).filter(Address.user_id == user_data.id).first()
+    if address_exists:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Address for this user already exists")
+
     try:
         to_address_encode = address.dict().copy()
         to_address_encode.update({'user_id': user_data.id})
@@ -27,13 +33,13 @@ async def create_address(address: AddressCreate, db: Session = Depends(get_db), 
 
         db.commit()
         db.refresh(new_address)
-
         return new_address
+
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e.__cause__))
 
 
-@router.get('/get-all', status_code=status.HTTP_200_OK)
+@router.get('/get-all', status_code=status.HTTP_200_OK, tags=['ADDRESS'])
 async def get_addresss(db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     try:
         address = db.query(Address).all()
@@ -42,7 +48,7 @@ async def get_addresss(db: Session = Depends(get_db), user_data: any = Depends(o
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e.__cause__))
 
 
-@router.get('/get-by-id/{id}', response_model=AddressGet, status_code=status.HTTP_200_OK)
+@router.get('/get-by-id/{id}', response_model=AddressGet, status_code=status.HTTP_200_OK, tags=['ADDRESS'])
 async def get_address_by_id(id: int, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     address = db.query(Address).filter(Address.id == id).first()
     if not address:
@@ -51,9 +57,10 @@ async def get_address_by_id(id: int, db: Session = Depends(get_db), user_data: a
     return address
 
 
-@router.put('/update/{id}', response_model=AddressUpdate, status_code=status.HTTP_200_OK)
+@router.patch('/update/{id}', response_model=AddressUpdate, status_code=status.HTTP_200_OK, tags=['ADDRESS'])
 async def update_address_by_id(id: int, address: AddressUpdate, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     address_db = db.query(Address).filter(Address.id == id).first()
+
     if not address_db:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Address not found")
 
@@ -70,7 +77,27 @@ async def update_address_by_id(id: int, address: AddressUpdate, db: Session = De
     return address_db
 
 
-@router.delete('/delete/{id}', status_code=status.HTTP_200_OK)
+@router.patch('/update-my-address', response_model=AddressUpdate, status_code=status.HTTP_200_OK, tags=['MY-ADDRESS'])
+async def update_my_address(address: AddressUpdate, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+    address_db = db.query(Address).filter(Address.id == user_data.id).first()
+
+    if not address_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Address not found")
+
+    address_db.pais = address.pais if address.pais else address_db.pais
+    address_db.estado = address.estado if address.estado else address_db.estado
+    address_db.municipio = address.municipio if address.municipio else address_db.municipio
+    address_db.cep = address.cep if address.cep else address_db.cep
+    address_db.rua = address.rua if address.rua else address_db.rua
+    address_db.numero = address.numero if address.numero else address_db.numero
+    address_db.complemento = address.complemento if address.complemento else address_db.complemento
+    address_db.user_id = address.user_id if address.user_id else address_db.user_id
+
+    db.commit()
+    return address_db
+
+
+@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, tags=['ADDRESS'])
 async def delete_address_by_id(id: int, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     address_db = db.query(Address).filter(Address.id == id)
     if not address_db.first():
@@ -81,7 +108,12 @@ async def delete_address_by_id(id: int, db: Session = Depends(get_db), user_data
     return {"detail": "Address deleted"}
 
 
-@router.get('/get-join/{id}', status_code=status.HTTP_200_OK)
-def get_address_by_user_id(id: int, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
-    join = db.query(User, Address).join(Address).filter(Address.user_id == id).all()
-    return join
+@router.delete('/delete-my-address', status_code=status.HTTP_200_OK, tags=['MY-ADDRESS'])
+async def delete_my_address(db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+    address_db = db.query(Address).filter(Address.id == user_data.id)
+    if not address_db.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Address not found")
+    address_db.delete(synchronize_session=False)
+    db.commit()
+
+    return {"detail": "Address deleted"}
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 #
-from app.models.models import User
+from app.models.models import Address, User
 from app.schemas.schemas import UserCreate, UserUpdate, UserGet
 from app.database.database import get_db
 # Utils
@@ -11,11 +11,11 @@ import time
 
 router = APIRouter(
     prefix="/users",
-    tags=['USERS']
+    # tags=['USERS']
 )
 
 
-@router.post('/create', response_model=UserCreate, status_code=status.HTTP_201_CREATED)
+@router.post('/create', response_model=UserCreate, status_code=status.HTTP_201_CREATED, tags=['USERS'])
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     try:
         pass_hashed = utils.hash(password=user.senha)
@@ -29,7 +29,7 @@ async def create_user(user: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e.__cause__))
 
 
-@router.get('/get-all', status_code=status.HTTP_200_OK)
+@router.get('/get-all', status_code=status.HTTP_200_OK, tags=['USERS'])
 async def get_users(db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     try:
         users = db.query(User).all()
@@ -38,7 +38,7 @@ async def get_users(db: Session = Depends(get_db), user_data: any = Depends(oaut
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e.__cause__))
 
 
-@router.get('/get-by-id/{id}', response_model=UserGet, status_code=status.HTTP_200_OK)
+@router.get('/get-by-id/{id}', response_model=UserGet, status_code=status.HTTP_200_OK, tags=['USERS'])
 async def get_user_by_id(id: int, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
 
     user = db.query(User).filter(User.id == id).first()
@@ -48,8 +48,15 @@ async def get_user_by_id(id: int, db: Session = Depends(get_db), user_data: any 
     return user
 
 
-@router.put('/update/{id}', response_model=UserUpdate, status_code=status.HTTP_200_OK)
+@router.get('/get-complete-info', status_code=status.HTTP_200_OK, tags=['USERS'])
+def get_users_with_address_associated(db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+    join = db.query(User, Address).join(Address).filter(Address.user_id == user_data.id).all()
+    return join
+
+
+@router.patch('/update/{id}', response_model=UserUpdate, status_code=status.HTTP_200_OK, tags=['USERS'])
 async def update_user_by_id(id: int, user: UserUpdate, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+
     user_db = db.query(User).filter(User.id == id).first()
 
     if not user_db:
@@ -64,9 +71,38 @@ async def update_user_by_id(id: int, user: UserUpdate, db: Session = Depends(get
     return user_db
 
 
-@router.delete('/delete/{id}', status_code=status.HTTP_200_OK)
+@router.patch('/update-my-user', response_model=UserUpdate, status_code=status.HTTP_200_OK, tags=['MY-USER'])
+async def update_my_user(user: UserUpdate, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+
+    user_db = db.query(User).filter(User.id == user_data.id).first()
+
+    if not user_db:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    user_db.nome = user.nome if user.nome else user_db.nome
+    user_db.email = user.email if user.email else user_db.email
+    user_db.cpf = user.cpf if user.cpf else user_db.cpf
+    user_db.pis = user.pis if user.pis else user_db.pis
+    user_db.senha = utils.hash(password=user.senha) if user.senha else user_db.senha
+    db.commit()
+    return user_db
+
+
+@router.delete('/delete/{id}', status_code=status.HTTP_200_OK, tags=['USERS'])
 async def delete_user_by_id(id: int, db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
     user_db = db.query(User).filter(User.id == id)
+
+    if not user_db.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    user_db.delete(synchronize_session=False)
+    db.commit()
+    return {"detail": "User deleted"}
+
+
+@router.delete('/delete-my-user', status_code=status.HTTP_200_OK, tags=['MY-USER'])
+async def delete_user_by_id(db: Session = Depends(get_db), user_data: any = Depends(oauth.get_user)):
+    user_db = db.query(User).filter(User.id == user_data.id)
 
     if not user_db.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
